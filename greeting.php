a4c56534b7bf02e636407f51e74be29d726430ba
Matthew Garcia <mgarcia@Matthews-MacBook-Pro-493.local>
2016-10-11 00:42:59 -0400

    greetingphp

---
+++ greeting.php
@@ -0,0 +1,7 @@
+<?php
+header("content-type: text/xml");
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+  <SpeakSentence locale="en_US" gender="female" voice="julie">Hello World! Welcome to Bandwidth XML!</SpeakSentence>
+</Response>
